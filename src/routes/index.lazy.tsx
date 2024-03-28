bd2b99b727fbc51518c4237f8c8d8bbfd9aef13b
godgodwinter <kakadz.pro@gmail.com>
2024-03-28 20:39:17 +0700

    update ui

--- src/routes/index.lazy.tsx
+++ src/routes/index.lazy.tsx
@@ -12,59 +12,52 @@ function Index() {
   )
 }
 
-
+import logo1 from '@/assets/mobycare/logo1.png'
 const LoginPages = ()=>{
   return(
     <>
     <div className="font-sans text-gray-900 antialiased p-2 px-4 ">
             <div className="min-h-screen flex flex-col sm:justify-start items-center pt-6 sm:pt-0 bg-[#f8f4f3]">
-                <div>
-                    <a href="/">
-                        <h2 className="font-bold text-3xl">MOBY <span className="bg-[#f84525] text-white px-2 rounded-md">CARE</span></h2>
-                    </a>
+                            <div>
+                                <div className="avatar">
+            <div className="w-24 mask mask-squircle">
+                <img src={logo1} />
+            </div>
+            </div>
                 </div>
 
                 <div className="w-full sm:max-w-md mt-6 px-6 py-4 bg-white shadow-md overflow-hidden sm:rounded-lg">
 
                         <div className="py-8">
                             <center>
-                                <span className="text-2xl font-semibold">Sign in to AdminPanel</span>
+                                <span className="text-2xl font-semibold">Selamat Datang di </span>
+                        <h2 className="font-bold text-3xl">MOBY <span className="bg-[#f84525] text-white px-2 rounded-md">CARE</span></h2>
+                        {/* add space on paragraph */}
+                        <p className='text-sm leading-relaxed my-4'>Aplikasi pemantauan tumbuh kembang anak yang terintegrasi antara kesehatan ibu dan anak, pendidikan parenting dan psikologi anda</p>
                             </center>
                         </div>
 
-                        <div>
-                            <label className="block font-medium text-sm text-gray-700"  />
-                            <input type='email' 
-                                name='email'
-                                placeholder='Email'
-                                className="w-full rounded-md py-2.5 px-4 border text-sm outline-[#f84525]" />                        
-                        </div>
-
-
-                        <div className="mt-4">
-                            <label className="block font-medium text-sm text-gray-700"  />
-                            <div className="relative">
-                                <input id="password" type="password" name="password" placeholder="Password" required  className = 'w-full rounded-md py-2.5 px-4 border text-sm outline-[#f84525]'/>
-
-                                <div className="absolute inset-y-0 right-0 pr-3 flex items-center text-sm leading-5">
-                                    <button type="button" id="togglePassword" className="text-gray-500 focus:outline-none focus:text-gray-600 hover:text-gray-600">
-                                                            
-                                    </button>
-                                </div>
-                            </div>
-                        </div>
+                     
 
                  
-
-                        <div className="flex items-center justify-end mt-4">
+                        <div className='flex justify-center'>     
+    <ul className="steps steps-horizontal">
+  <li className="step step-primary"></li>
+  <li className="step "></li>
+  <li className="step"></li>
+  <li className="step"></li>
+</ul></div>
+                        <div className="flex items-center justify-between mt-4">
                               
 
-                                <Link to="/admin/sekolah" className="ms-4 inline-flex items-center px-4 py-2 bg-[#f84525] border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-red-800 focus:bg-gray-700 active:bg-gray-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 transition ease-in-out duration-150">
-           Sign In
+                                <Link to="/login" className="ms-4 inline-flex items-center px-4 py-2 bg-[#f84525] border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-red-800 focus:bg-gray-700 active:bg-gray-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 transition ease-in-out duration-150">
+           Skip
         </Link>{' '}
-                            {/* <button className= 'ms-4 inline-flex items-center px-4 py-2 bg-[#f84525] border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-red-800 focus:bg-gray-700 active:bg-gray-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 transition ease-in-out duration-150'>
-                                Sign In
-                            </button> */}
+   
+   
+                                <Link to="/admin/sekolah" className="ms-4 inline-flex items-center px-4 py-2 bg-[#f84525] border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-red-800 focus:bg-gray-700 active:bg-gray-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 transition ease-in-out duration-150">
+           Next
+        </Link>{' '}
 
                         </div>
                         
--- src/routes/login.lazy.tsx
+++ src/routes/login.lazy.tsx
@@ -1,45 +1,81 @@
-import { createLazyFileRoute } from '@tanstack/react-router';
+import { Link, createLazyFileRoute } from '@tanstack/react-router';
 
 export const Route = createLazyFileRoute('/login')({
-  component: About,
+  component: Index,
 })
 
-function About() {
-  return <div className="p-2">
-    <Basic/>
-  </div>
+function Index() {
+  return (
+    <>
+    <LoginPages/>
+    </>
+  )
 }
 
-import * as React from 'react';
- import {
-   Formik,
-   Form,
-   Field,
- } from 'formik';
- 
- interface MyFormValues {
-   firstName: string;
- }
- 
-  const Basic: React.FC<{}> = () => {
-   const initialValues: MyFormValues = { firstName: '' };
-   return (
-     <div>
-       <h1>My Example</h1>
-       <Formik
-         initialValues={initialValues}
-         onSubmit={(values, actions) => {
-           console.log({ values, actions });
-           alert(JSON.stringify(values, null, 2));
-           actions.setSubmitting(false);
-         }}
-       >
-         <Form>
-           <label htmlFor="firstName">First Name</label>
-           <Field id="firstName" name="firstName" placeholder="First Name" className="input"/>
-           <button type="submit" className='btn btn-info btn-sm'>Submit</button>
-         </Form>
-       </Formik>
-     </div>
-   );
- };
+import logo1 from '@/assets/mobycare/logo1.png'
+const LoginPages = ()=>{
+  return(
+    <>
+    <div className="font-sans text-gray-900 antialiased p-2 px-4 ">
+            <div className="min-h-screen flex flex-col sm:justify-start items-center pt-6 sm:pt-0 bg-[#f8f4f3]">
+                <div>
+                    {/* <a href="/">
+                        <h2 className="font-bold text-3xl">MOBY <span className="bg-[#f84525] text-white px-2 rounded-md">CARE</span></h2>
+                    </a> */}
+                    <div className="avatar">
+  <div className="w-24 mask mask-squircle">
+    <img src={logo1} />
+  </div>
+</div>
+                </div>
+
+                <div className="w-full sm:max-w-md mt-6 px-6 py-4 bg-white shadow-md overflow-hidden sm:rounded-lg">
+
+                        <div className="py-8">
+                            <center>
+                                <span className="text-2xl font-semibold">Sign in to AdminPanel</span>
+                            </center>
+                        </div>
+
+                        <div>
+                            <label className="block font-medium text-sm text-gray-700"  />
+                            <input type='email' 
+                                name='email'
+                                placeholder='Email'
+                                className="w-full rounded-md py-2.5 px-4 border text-sm outline-[#f84525]" />                        
+                        </div>
+
+
+                        <div className="mt-4">
+                            <label className="block font-medium text-sm text-gray-700"  />
+                            <div className="relative">
+                                <input id="password" type="password" name="password" placeholder="Password" required  className = 'w-full rounded-md py-2.5 px-4 border text-sm outline-[#f84525]'/>
+
+                                <div className="absolute inset-y-0 right-0 pr-3 flex items-center text-sm leading-5">
+                                    <button type="button" id="togglePassword" className="text-gray-500 focus:outline-none focus:text-gray-600 hover:text-gray-600">
+                                                            
+                                    </button>
+                                </div>
+                            </div>
+                        </div>
+
+                 
+
+                        <div className="flex items-center justify-end mt-4">
+                              
+
+                                <Link to="/admin/sekolah" className="ms-4 inline-flex items-center px-4 py-2 bg-[#f84525] border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-red-800 focus:bg-gray-700 active:bg-gray-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 transition ease-in-out duration-150">
+           Sign In
+        </Link>{' '}
+                            {/* <button className= 'ms-4 inline-flex items-center px-4 py-2 bg-[#f84525] border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-red-800 focus:bg-gray-700 active:bg-gray-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 transition ease-in-out duration-150'>
+                                Sign In
+                            </button> */}
+
+                        </div>
+                        
+                </div>
+            </div>
+        </div>
+    </>
+  )
+}
